f2c2fbc560932ea2f19c0b518ca725009e0c60b3
Jeet <jeeth.enterprises108@gmail.com>
2025-02-11 23:43:06 +0530

    Update download.js

--- js/download.js
+++ js/download.js
@@ -1,11 +1,11 @@
-document.getElementById('downloadPDF').addEventListener('click', () => {
-    const pdfUrl = "https://sangramjeetghosh-cv.tiiny.site"; // Replace with your actual PDF link
+// document.getElementById('downloadPDF').addEventListener('click', () => {
+//     const pdfUrl = "https://sangramjeetghosh-cv.tiiny.site"; // Replace with your actual PDF link
 
-    const link = document.createElement("a");
-    link.href = pdfUrl;
-    link.target = "_blank"; // Opens in a new tab
-    link.download = "Invoice.pdf"; // Suggested filename for download
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-});
+//     const link = document.createElement("a");
+//     link.href = pdfUrl;
+//     link.target = "_blank"; // Opens in a new tab
+//     link.download = "Invoice.pdf"; // Suggested filename for download
+//     document.body.appendChild(link);
+//     link.click();
+//     document.body.removeChild(link);
+// });
